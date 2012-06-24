4c20141df7d871ca3a88f20c436503211bab1cc5
Keith Johnson <kj@ubergeek42.com>
2012-06-24 16:33:32 -0400

    Add supporting files for login

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,15 @@
 define(function(require) {
+    var $ = require('jquery');
     var socket = io.connect('http://localhost:8000');
     socket.on('print', function(data) {
-        document.getElementById('msg').innerHTML = data.msg;
+        $('#msg').text(data.msg);
+    });
+
+    $('#loginform').on('submit', function(e){
+        e.preventDefault();
+        console.log('loginsubmitted');
+        socket.emit('login', {username: $('#username').val()});
+        return false;
     });
 });
+
